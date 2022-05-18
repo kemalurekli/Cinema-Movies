b97fcd644ff5cd9476dfa74a1bfff9fa3f21bbad
kemalurekli <kemalurekli@gmail.com>
2022-05-18 14:21:57 +0300

    Layout design created.

--- app/src/main/java/com/kemalurekli/cinema/view/MainActivity.kt
+++ app/src/main/java/com/kemalurekli/cinema/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kemalurekli.cinema
+package com.kemalurekli.cinema.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kemalurekli.cinema.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
